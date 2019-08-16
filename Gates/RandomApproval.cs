346ce2f3423fe2211fcd82ca9e308c69d1c2ed5f
Jörg Jooss <joerg@joergjooss.de>
2019-08-16 10:11:37 +0200

    Add project files.

---
+++ Gates/RandomApproval.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Gates
+{
+    public class RandomApproval
+    {
+        private readonly Random _random;
+
+        public RandomApproval(Random random)
+        {
+            _random = random ?? throw new ArgumentNullException(nameof(random));
+        }
+
+        [FunctionName(nameof(RandomApproval))]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest request,
+            ILogger logger)
+        {
+            logger.LogInformation("RandomApproval triggered.");
+
+            string requestBody;
+
+            using (var reader = new StreamReader(request.Body))
+            {
+                requestBody = await reader.ReadToEndAsync();
+            }
+            logger.LogInformation("Body: {Body}", requestBody);
+
+            int i = _random.Next(0, 2);
+            return (i == 1) ? new OkResult() : new StatusCodeResult(StatusCodes.Status412PreconditionFailed);
+        }
+    }
+}
